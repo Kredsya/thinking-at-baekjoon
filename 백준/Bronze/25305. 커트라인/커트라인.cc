f009dcbd6c43d79e5c954e9e232a21dde03edaff
Doyeop Kim <do901328@gmail.com>
2023-02-27 02:09:38 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define LOAD_TXT freopen("input.txt", "r", stdin)
+using namespace std;
+using ll = long long;
+
+int main() {
+#ifndef BOJ
+    LOAD_TXT;
+#else
+    FAST_IO;
+#endif
+    
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    reverse(a.begin(), a.end());
+    cout << a[k-1];
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
