763daa6b64a7e17ed93ba38f594a911febd028d6
Doyeop Kim <do901328@gmail.com>
2023-08-05 16:00:26 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define LOAD_TXT freopen("input.txt", "r", stdin);
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+using ll = long long;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    LOAD_TXT
+#else
+    FAST_IO
+#endif
+    
+    int n;
+    cin >> n;
+    while(n--)
+        cout << "SciComLove\n";
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
