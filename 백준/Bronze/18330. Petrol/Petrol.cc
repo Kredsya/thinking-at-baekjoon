6f7a9c350514fa830dedfec18ff5ac4f8b23c0ba
Doyeop Kim <do901328@gmail.com>
2023-08-18 21:04:17 +0900

    [Bronze IV] Title: Petrol, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18330. Petrol/Petrol.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define LOAD_TXT freopen("input.txt", "r", stdin)
+#define int1(a) int a;cin>>a;
+#define int2(a,b) int a,b;cin>>a>>b;
+#define int3(a,b,c) int a,b,c;cin>>a>>b>>c;
+#define ints(v,n) std::vector<int>v(n);for(int i=0;i<n;i++)cin>>v[i];
+using namespace std;
+using ll = long long;
+
+int main() {
+#ifndef BOJ
+    LOAD_TXT;
+#else
+    FAST_IO;
+#endif
+    
+    int2(n,k)
+    k+=60;
+    if(n<=k) {
+        cout << 1500*n;
+    }
+    else {
+        cout << 1500*(k) + 3000*(n-k);
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
