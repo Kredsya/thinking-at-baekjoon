919ed70aa617679560b811fd03b4864e76006a66
Kredsya <do901328@gmail.com>
2024-09-17 04:47:34 +0900

    [Silver II] Title: 예산, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ C++14/백준/Silver/2512. 예산/예산.cc
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define LOAD_TXT freopen("input.txt", "r", stdin)
+#define int1(a) int a;cin>>a;
+#define int2(a,b) int a,b;cin>>a>>b;
+#define int3(a,b,c) int a,b,c;cin>>a>>b>>c;
+#define ints(v,n) vector<int>v(n);for(int i=0;i<n;i++)cin>>v[i];
+#define vsort(v) sort(v.begin(),v.end());
+#define csort(v,f) sort(v.begin(),v.end(),f);
+#define x first
+#define y second
+using namespace std;
+using ll = long long;
+
+int n, m;
+vector<int> v;
+
+bool check(int mid) {
+    int sum = 0;
+    for(int i=0; i<n; i++) {
+        sum += min(v[i], mid);
+    }
+    if(sum > m) return false;
+    return true;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    LOAD_TXT;
+#else
+    FAST_IO;
+#endif
+    
+    cin >> n;
+    v = vector<int>(n);
+    for(int i=0; i<n; i++) {
+        cin >> v[i];
+    }
+    cin >> m;
+    
+    int l = 0, h = *max_element(v.begin(), v.end()), mid, ans = 0;
+    while(l <= h) {
+        mid = (l + h) / 2;
+        if(check(mid)) {
+            l = mid + 1;
+            ans = max(ans, mid);
+        }
+        else {
+            h = mid - 1;
+        }
+    }
+    cout << ans;
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
