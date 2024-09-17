7ce13318b1525f36f3e8e275859d9e16208dae14
Kredsya <do901328@gmail.com>
2024-09-17 20:44:20 +0900

    [Gold V] Title: 입국심사, Time: 56 ms, Memory: 2412 KB -BaekjoonHub

---
+++ C++14/백준/Gold/3079. 입국심사/입국심사.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define LOAD_TXT freopen("input.txt", "r", stdin)
+#define int1(a) int a;cin>>a;
+#define int2(a,b) int a,b;cin>>a>>b;
+#define int3(a,b,c) int a,b,c;cin>>a>>b>>c;
+#define ints(v,n) vector<int>v(n);for(int i=0;i<n;i++)cin>>v[i];
+#define vsort(v) sort(v.begin(),v.end());
+#define csort(v,f) sort(v.begin(),v.end(),f);
+#define x first
+#define y second
+using namespace std;
+using ll = unsigned long long;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    LOAD_TXT;
+#else
+    FAST_IO;
+#endif
+    
+    int2(n, m);
+    ints(v, n);
+    
+    ll l = 1, h = 1'000'000'000 * 1'000'000'000;
+    ll ans = h, mid, cnt;
+    while(l <= h) {
+        cnt = 0;
+        mid = (l + h) / 2;
+        for(int i=0; i<n; i++) {
+            cnt += mid / v[i];
+        }
+        
+        if(cnt >= m) {
+            h = mid - 1;
+            ans = mid;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+    
+    cout << ans;
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
