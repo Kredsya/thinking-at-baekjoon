20f33e58584ecfcdeff5f8c64f172933c6a5dfd2
Kredsya <do901328@gmail.com>
2025-03-20 18:07:13 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++14/백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define LOAD_TXT freopen("input.txt", "r", stdin)
+#define int1(a) int a;cin>>a;
+#define int2(a,b) int a,b;cin>>a>>b;
+#define int3(a,b,c) int a,b,c;cin>>a>>b>>c;
+#define ints(v,n) vector<int>v(n);for(int i=0;i<n;i++)cin>>v[i];
+#define vsort(v) sort(v.begin(),v.end());
+#define csort(v,f) sort(v.begin(),v.end(),f);
+#define x first
+#define y second
+using namespace std;
+using ll = long long;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    LOAD_TXT;
+#else
+    FAST_IO;
+#endif
+    
+    int1(n)
+    for(int i=0; i<n; i++) {
+        cout << "LoveisKoreaUniversity ";
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
