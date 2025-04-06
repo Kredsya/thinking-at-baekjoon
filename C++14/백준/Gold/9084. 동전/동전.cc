ceab9810d657010b548a947a9430d5fc68eec564
kredsya <do901328@gmail.com>
2025-04-07 05:19:09 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ C++14/백준/Gold/9084. 동전/동전.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define LOAD_TXT freopen("input.txt", "r", stdin)
+#define int1(a) int a;cin>>a;
+#define int2(a,b) int a,b;cin>>a>>b;
+#define int3(a,b,c) int a,b,c;cin>>a>>b>>c;
+#define ints(v,n) vector<int>v(n);for(int i=0;i<n;i++)cin>>v[i];
+#define vsort(v) sort(v.begin(),v.end());
+#define csort(v,f) sort(v.begin(),v.end(),f);
+#define x first
+#define y second
+using namespace std;
+using ll = long long;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    LOAD_TXT;
+#else
+    FAST_IO;
+#endif
+    
+    int1(t)
+    while(t--) {
+        int1(n)
+        ints(coins, n)
+        int1(m)
+        vector<int> dp(m + 1);
+        dp[0] = 1;
+        for(auto coin : coins) {
+            for(int i=coin; i<=m; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        cout << dp[m] << '\n';
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
