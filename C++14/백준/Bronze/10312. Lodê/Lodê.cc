cd47eff5bf0c487d27d59d130fa714edefd02d70
kredsya <do901328@gmail.com>
2025-04-07 04:42:39 +0900

    [Bronze II] Title: Lodê, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++14/백준/Bronze/10312. Lodê/Lodê.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define LOAD_TXT freopen("input.txt", "r", stdin)
+#define int1(a) int a;cin>>a;
+#define int2(a,b) int a,b;cin>>a>>b;
+#define int3(a,b,c) int a,b,c;cin>>a>>b>>c;
+#define ints(v,n) vector<int>v(n);for(int i=0;i<n;i++)cin>>v[i];
+#define vsort(v) sort(v.begin(),v.end());
+#define csort(v,f) sort(v.begin(),v.end(),f);
+#define x first
+#define y second
+using namespace std;
+using ll = long long;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    LOAD_TXT;
+#else
+    FAST_IO;
+#endif
+    
+    // 1, 3, 9, 27, ...
+    
+    int1(n)
+    while(n--) {
+        int1(k)
+        int i = 1;
+        while(i * 3 <= k) i *= 3;
+        while(i > 0) {
+            cout << k / i << ' ';
+            k -= (k / i) * i;
+            i /= 3;
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
